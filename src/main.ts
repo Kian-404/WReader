8185370306695f12ad2ad62504c92576c45c3951
kian-404 <tianjianen2015@gmail.com>
2023-07-01 19:29:44 +0800

    ✨ feat: add vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
 import { createApp } from "vue";
-import "./styles.css";
+import "./styles/styles.css";
 import App from "./App.vue";
+import {router} from './routers/index'
+import { createPinia } from "pinia";
 
-createApp(App).mount("#app");
+const pinia = createPinia();
+
+const app = createApp(App);
+
+app.use(pinia);
+
+app.use(router);
+
+
+app.mount("#app");
