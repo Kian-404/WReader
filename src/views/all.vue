8dc383331e64ae4bfdbb87db41ec90bd9647aebc
kian-404 <tianjianen2015@gmail.com>
2023-07-03 22:13:10 +0800

    ✨ feat: test upload local

--- src/views/all.vue
+++ src/views/all.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
-    all
+    <n-card v-for="item in bookList" title="卡片">
+      {{ item }}
+    </n-card>
   </div>
 </template>
 
 <script setup lang="ts">
-
+import * as localforage from 'localforage'
+import { ref } from 'vue';
+let bookList=  ref<string[]>([]);
+localforage.keys().then(function(keys) {
+    // 包含所有 key 名的数组
+    console.log(keys);
+    bookList.value = keys;
+}).catch(function(err) {
+    // 当出错时，此处代码运行
+    console.log(err);
+});
 </script>
 
 <style scoped>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,11 +1,56 @@
 <template>
   <div>
-    home
+    <n-upload
+    ref="uploadFile"
+    action="undefined"
+    :on-before-upload="handleBefore"
+    :on-change="changFile"
+    :custom-request="customRequest"
+    :show-file-list="false"
+  >
+    <n-upload-dragger>
+      <div style="margin-bottom: 12px">
+        <n-icon size="48" :depth="3">
+          <archive-icon />
+        </n-icon>
+      </div>
+      <n-text style="font-size: 16px">
+        点击或者拖动文件到该区域来上传
+      </n-text>
+      <n-p depth="3" style="margin: 8px 0 0 0">
+        请不要上传敏感数据，比如你的银行卡号和密码，信用卡号有效期和安全码
+      </n-p>
+    </n-upload-dragger>
+  </n-upload>
   </div>
 </template>
 
 <script setup lang="ts">
-
+import { UploadCustomRequestOptions, UploadFileInfo } from 'naive-ui/es/components';
+import { ref } from 'vue';
+import * as localforage from 'localforage'
+const uploadFile = ref(null);
+const handleBefore = () => {
+  console.log('upload before')
+  console.log((uploadFile as any).value.clear())
+}
+const changFile = (file: UploadFileInfo) => {
+  console.log(file)
+}
+const customRequest = ({
+      file,
+      data,
+      headers,
+      withCredentials,
+      action,
+      onFinish,
+      onError,
+      onProgress
+    }: UploadCustomRequestOptions) => {
+      console.log(file);
+      let fileName =  file.name.split('.').shift()|| '';
+      localforage.setItem( fileName, file.file);
+    }
 </script>
 
 <style scoped>
